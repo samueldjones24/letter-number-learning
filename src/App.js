f70bdb3b1b4d95492cbb2b959871b7e3e3e2c5c6
Sam Jones <samueldjones24@gmail.com>
2022-03-04 15:52:46 +0000

    Add sounds + image files, start homepage with links and buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Link to="/">
+          <h2 className="header-title">Alphabet & Numbers Learning</h2>
+        </Link>
       </header>
+      <body>
+        <p>Choose your activity:</p>
+        <div className="activity-buttons">
+          <Link to="/letters">
+            <button type="button">Letters</button>
+          </Link>
+          <Link to="/numbers">
+            <button type="button">Numbers</button>
+          </Link>
+        </div>
+      </body>
+      <footer>
+        <div>
+          <p>
+            Images: ©{" "}
+            <a
+              href="https://alphabetimals.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="external-link"
+            >
+              Alphabetimals
+            </a>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
